c9bf7abd2a0eff3d6c4684d5ae9c40af819934c8
Ton Binh On <nick.on437@gmail.com>
2020-07-25 12:52:13 +1000

    [gallery-start] Complete script of gallery-start

--- javascript/building-blocks/gallery-start/main.js
+++ javascript/building-blocks/gallery-start/main.js
@@ -6,8 +6,25 @@ const overlay = document.querySelector('.overlay');
 
 /* Looping through images */
 
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-thumbBar.appendChild(newImage);
+for (let i = 1; i <= 5; i++) {
+    const newImage = document.createElement('img');
+    const imgSrc = 'images/pic' + i + '.jpg'
+    newImage.setAttribute('src', imgSrc);
+    thumbBar.appendChild(newImage);
+    newImage.addEventListener('click', function() {
+        displayedImage.setAttribute('src', imgSrc);
+    });
+}
 
 /* Wiring up the Darken/Lighten button */
+btn.addEventListener('click', function() {
+    if (btn.getAttribute('class') == 'dark') {
+        btn.setAttribute('class', 'light');
+        btn.textContent = 'lighten';
+        overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
+    } else {
+        btn.setAttribute('class', 'dark');
+        btn.textContent = 'darken';
+        overlay.style.backgroundColor = 'rgba(0,0,0,0)';
+    }
+});
